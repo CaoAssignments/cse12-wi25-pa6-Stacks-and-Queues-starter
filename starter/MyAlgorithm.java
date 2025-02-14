a9a89b57f8aa757f0a99b9bb79da1d3f16afc573
Hezi Zhang <zhhz1994@gmail.com>
2025-02-13 22:43:16 -0800

    update starter and readme

---
+++ starter/MyAlgorithm.java
@@ -0,0 +1,15 @@
+/**
+ * This class contains an algorithm utilizing a stack or queue. 
+ */
+public class MyAlgorithm {
+    /**
+     * Returns whether or not the given string contains a valid arrangement of brackets
+     * 
+     * @param input the input string containing brackets
+     * @throws NullPointerException if the given string is null
+     * @return the whether or not the given string contains a valid arrangement of brackets
+     */
+    public static boolean isValidBrackets(String input) {
+        return 0;
+    }
+}
